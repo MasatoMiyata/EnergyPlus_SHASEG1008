9b61207e414f5051110e66950d1d5687eaa5071b
MasatoMiyata <myt.masato@gmail.com>
2021-11-19 13:31:47 +0900

    削除するファイルの追加

--- idf_miyata/energyplus_file_delete.py
+++ idf_miyata/energyplus_file_delete.py
@@ -9,7 +9,7 @@ files_file = [f for f in files if os.path.isfile(os.path.join(path, f))]
 # 不要なファイルの削除
 for filename in files_file:
 
-    for extension in [".audit", ".bnd", "dxf", "eio", "err", "eso", "mtd", "rvaudit", "shd", "svg", "mtr", "Zsz.csv", "mtr" ]:
+    for extension in [".audit", ".bnd", "dxf", "eio", "err", "eso", "mtd", "rvaudit", "shd", "svg", "mtr", "Zsz.csv", "mtr", ".rdd", ".mdd" ]:
     
         if filename.endswith(extension):
             os.remove(path + "/" + filename)
